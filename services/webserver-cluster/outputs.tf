7bcb5e14c12f415da1073ad8e16a61538ba3bef8
toshiki-otaka <t.ohtaka@soda-inc.jp>
2024-01-17 00:20:17 +0900

    delete duplicated output

--- services/webserver-cluster/outputs.tf
+++ services/webserver-cluster/outputs.tf
@@ -1,8 +1,3 @@
-output "alb_dns_name" {
-  value = aws_lb.example.dns_name
-  description = "The domain name of the load balancer"
-}
-
 output "asg_name" {
   value = aws_autoscaling_group.example.name
   description = "The name of the autoscaling group"
