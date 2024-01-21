63a01ef5c1c24e98295969df82b37e5c9b447ef4
toshiki-otaka <t.ohtaka@soda-inc.jp>
2024-01-22 08:56:09 +0900

    dynamic tags

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -48,6 +48,16 @@ resource "aws_autoscaling_group" "example" {
     value = "${var.cluster_name}-example"
     propagate_at_launch = true
   }
+
+  dynamic "tag" {
+    for_each = var.custom_tags
+
+    content {
+      key = tag.key
+      value = tag.value
+      propagate_at_launch = true
+    }
+  }
 }
 
 resource "aws_security_group" "instance" {
